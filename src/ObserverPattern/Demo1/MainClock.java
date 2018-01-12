c89b3107a36798af5870c12e2df2b19010354c70
joie <joie_chen9@hotmail.com>
2018-01-12 17:13:10 +0800

    Created and implemented method MainClock.makeHourWithin0To23().

--- src/ObserverPattern/Demo1/MainClock.java
+++ src/ObserverPattern/Demo1/MainClock.java
@@ -20,4 +20,8 @@ public abstract class MainClock {
     public void setLocalTimeFromUtcZeroTime(int utcZeroTime) {
         this.localTime = MainClock.makeHourWithin0To23(utcZeroTime + this.UTC_OFFSET);
     }
+
+    private static int makeHourWithin0To23(int hour) {
+        return (hour + 24) % 24;
+    }
 }

1ee0f56cc414413d858c69979f31ce316b0e5db1
joie <joie_chen9@hotmail.com>
2018-01-12 15:55:34 +0800

    Implemented method CityClock.setLocalTime().

--- src/ObserverPattern/Demo1/CityClock.java
+++ src/ObserverPattern/Demo1/CityClock.java
@@ -3,5 +3,9 @@ package ObserverPattern.Demo1;
 /**
  * Created by test on 18/1/12.
  */
-public class CityClock {
+public class CityClock extends MainClock{
+    @Override
+    public void setLocalTime(int localTime) {
+        super.localTime = localTime;
+    }
 }

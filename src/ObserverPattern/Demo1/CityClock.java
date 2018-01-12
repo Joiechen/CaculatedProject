a86d30fda5780f637272f1b7e88b63089cb4cb9b
joie <joie_chen9@hotmail.com>
2018-01-12 16:25:47 +0800

    Added constructor CityClock(int utcOffset).

--- src/ObserverPattern/Demo1/CityClock.java
+++ src/ObserverPattern/Demo1/CityClock.java
@@ -4,6 +4,10 @@ package ObserverPattern.Demo1;
  * Created by test on 18/1/12.
  */
 public class CityClock extends MainClock{
+    public CityClock(int utcOffset) {
+        super();
+    }
+
     @Override
     public void setLocalTime(int localTime) {
         super.localTime = localTime;

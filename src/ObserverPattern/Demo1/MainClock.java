8f000c7947476616900c5ea5171e66a86064d7ac
joie <joie_chen9@hotmail.com>
2018-01-12 15:29:11 +0800

    Added static method MainClock.toLocalTime().

--- src/ObserverPattern/Demo1/MainClock.java
+++ src/ObserverPattern/Demo1/MainClock.java
@@ -4,8 +4,12 @@ package ObserverPattern.Demo1;
  * Created by test on 18/1/12.
  */
 public abstract class MainClock {
-    private final int UTC_OFFSET = 0;
+    private static final int UTC_OFFSET = 0;
     private int localTime = 0;
 
     public abstract void setLocalTime(int localTime);
+
+    public static int toLocalTime(int utcZeroTime) {
+        return utcZeroTime + UTC_OFFSET;
+    }
 }

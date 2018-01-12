df4ac67ce4f2852b320fc75f272e5b1eebe36835
joie <joie_chen9@hotmail.com>
2018-01-12 10:10:29 +0800

    Wrote class MainClock accroding to the class diagram.

--- src/ObserverPattern/Demo1/MainClock.java
+++ src/ObserverPattern/Demo1/MainClock.java
@@ -3,5 +3,9 @@ package ObserverPattern.Demo1;
 /**
  * Created by test on 18/1/12.
  */
-public class MainClock {
+public abstract class MainClock {
+    private final int UTC_OFFSET = 0;
+    private int localTime = 0;
+
+    public abstract void setLocalTime(int localTime);
 }
